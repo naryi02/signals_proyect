dda8da80824924f8d939f12b3417766646a02eae
naryi02 <102570693+naryi02@users.noreply.github.com>
2023-06-01 19:30:17 -0500

    Add files via upload

--- Proyecto_finalSS.m
+++ Proyecto_finalSS.m
@@ -1,14 +1,12 @@
 %Reading audio files
 [x1, fs] =audioread(uigetfile('.wav'));
 sound(x1,fs)
-
 N = length(x1); % Número de muestras de la señal.
 Td = N/fs; % Tiempo de duración.
 Ts = 1/fs; % Periodo de muestreo. 
 NUM = round(Td/15.5,3);
 duration_seg = NUM; %duración del segmento
 nT_seg = duration_seg * fs;%Estimación números de muestras por segmento
-
 total_seg = floor(length(x1) / nT_seg);%Numero total de segmentos
 
 segmentos = reshape(x1(1:total_seg*nT_seg), nT_seg, total_seg);%Segmentación de la señal inicial (AUDIO)
@@ -49,17 +47,12 @@ indice_alta_frecuencia = find(f >= 1000 , 1);
 indice_max_alta_frecuencia = indice_max_alta_frecuencia + indice_alta_frecuencia - 1;
 frecuencia_alta = f(indice_max_alta_frecuencia);
 
-% % Calculate the allowable frequency range for low and high tones
-% variationLow = frecuencia_baja * variationPercentage / 100;
-% variationHigh = frecuencia_alta * variationPercentage / 100;
-
 % Define the DTMF tones and their corresponding frequencies
 dtmfTones = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '*', '#'];
 lowFreq = [ 697, 697, 697, 770, 770, 770, 852, 852, 852, 941, 941, 941 ];
 highFreq = [ 1209, 1336, 1477, 1209, 1336, 1477, 1209, 1336, 1477, 1336, 1209, 1477 ];
 
-for j = 1:length(dtmfTones)
- 
+for j = 1:length(dtmfTones) 
 % Calculate the allowable frequency range for low and high tones
 lowRange = [lowFreq(j) - lowFreq(j)*0.015, lowFreq(j) + lowFreq(j)*0.015];
 highRange = [highFreq(j) - highFreq(j)*0.015, highFreq(j) + highFreq(j)*0.015];
